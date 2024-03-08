028f7654db876dbd21a11185b3c2b8b0574af64b
DevangiKanani <devangikanani123@gmail.com>
2024-03-08 12:56:37 +0530

    update app.js

--- app.js
+++ app.js
@@ -7,6 +7,26 @@ conect();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: ['http://localhost:4200', 'http://tunepays.net', 'https://tunepays.net', 'http://www.tunepays.net', 'https://www.tunepays.net', 'http://tunepays.org', 'https://tunepays.org', 'http://tunepays.com', 'https://tunepays.com', 'http://www.tunepays.com', 'https://www.tunepays.com','https://web.tunepays.com/'],
+  optionsSuccessStatus: 200
+}));
+
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://localhost:4200', 'http://tunepays.net', 'https://tunepays.net', 'http://www.tunepays.net', 'https://www.tunepays.net', 'http://tunepays.org', 'https://tunepays.org', 'http://tunepays.com', 'https://tunepays.com', 'http://www.tunepays.com', 'https://www.tunepays.com','https://web.tunepays.com/'];
+  
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);  
+  res.setHeader('Set-Cookie', 'flavor=choco; SameSite=None; Secure');
+  return next();
+});
+
 app.use('/', routes)
 
 app.use(errorHandler)
